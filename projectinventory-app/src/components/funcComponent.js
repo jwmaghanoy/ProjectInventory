732377a0bbc86c55de04a04771ef6bef37a6e8db
Joie Ann Maghanoy <joieannmaghanoy@Joies-MacBook-Pro.local>
2021-05-04 16:21:27 +0800

    add inventory

--- projectinventory-app/src/components/funcComponent.js
+++ projectinventory-app/src/components/funcComponent.js
@@ -1,15 +1,30 @@
-import {Form, Button } from "react-bootstrap";
-
+import {Form, Button, Table } from "react-bootstrap";
+import { useState} from 'react';
 export default function AddProduct(){
     //add product handler method
+    let initialValue = [];
+    const [products, setProduct ] = useState(initialValue);
+    //products = []
     const add = (event)=>{
         event.preventDefault(); // to check the console
         //console.log(event.target); // to check the line of form in the console
-        console.log(event.target.product_name.value);
+        //console.log(event.target.product_name.value);
+
+        const formData = event.target;
+        const newProduct = {
+            product_name: formData.product_name.value,
+            price: formData.price.value,
+            qty: formData.qty.value
+
+        }
+        //console.log(newProduct); 
+        // add a new product inside products array
+        setProduct([...products, newProduct]); 
+        console.log(products);
     }
     return (
         <div>
-            <Form onSubmit={add}> // coming from const add
+            <Form onSubmit={add}> 
   <Form.Group controlId="formBasicProduct">
     <Form.Label>Product Name:</Form.Label>
     <Form.Control type="text" placeholder="Enter Product" name="product_name"/>
@@ -17,7 +32,7 @@ export default function AddProduct(){
 
   <Form.Group controlId="formBasicPrice">
     <Form.Label>Price</Form.Label>
-    <Form.Control type="number" placeholder="Price in Euro" name="price"/>
+    <Form.Control type="number" placeholder="Price in Peso" name="price"/>
   </Form.Group>
   
   <Form.Group controlId="formBasicQuantity">
@@ -30,6 +45,34 @@ export default function AddProduct(){
     Add to Inventory
   </Button>
 </Form>
+
+<Table striped bordered hover variant="dark">
+  <thead>
+    <tr>
+      <th>Index</th>
+      <th>Product Name</th>
+      <th>Price</th>
+      <th>Quantity</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+        products.map((item,index)=>
+        {
+            return(
+                <tr>
+                <td>{index}</td>
+                <td>{item.product_name}</td>
+                <td>{item.price}</td>
+                <td>{item.qty}</td>
+                </tr>
+            )
+
+        })
+    }
+    
+  </tbody>
+</Table>
         </div>
     )
 }
